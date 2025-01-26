38eed25c45ff712059ac98bef23178857c6758db
yama-consulting <backoffice@yamaconsulting.fr>
2025-01-26 23:16:33 +0100

    new updates

--- app/api/getCard/route.js
+++ app/api/getCard/route.js
@@ -1,14 +1,21 @@
 const cards = [
-  { id: 1, question: 'What is the capital of France?', answer: 'Paris' },
-    { id: 2, question: 'What is the color of the sky?', answer: 'Blue' },
-    { id: 3, question: 'What is 2 + 2?', answer: '4' },
-    { id: 4, question: 'What is the largest planet in our solar system?', answer: 'Jupiter' },
-    { id: 5, question: 'What is the square root of 64?', answer: '8' },
-    { id: 6, question: 'Who wrote the play "Romeo and Juliet"?', answer: 'William Shakespeare' },
-    { id: 7, question: 'What is the chemical symbol for water?', answer: 'H2O' },
-    { id: 8, question: 'What is the smallest country in the world?', answer: 'Vatican City' },
-    { id: 9, question: 'Who painted the Mona Lisa?', answer: 'Leonardo da Vinci' },
-    { id: 10, question: 'What is the capital of Japan?', answer: 'Tokyo' },
+  {"id":1,"word":"abandon","type":"(v)","ipa":"/ə'bændən/","cefr_level":"B2","en":"to stop an activity before it is finished, usually because you think you cannot succeed","example_en":"We have to abandon the plan.","ipa_example":"/wi həv tʊ əˈbændən ðə plæn/","vi":"abandonner","example_vi":"Nous devons abandonner le plan.","connect":"","image":"1P9y8yEbnkSHzT8Dy9RLNzTKiSNSmGyFQ"},
+  {"id":2,"word":"abandoned","type":"(adj)","ipa":"/ə'bændənd/","cefr_level":"B2","en":"left and no longer wanted, used or needed","example_en":"He hid in an abandoned building.","ipa_example":"/hiː hɪd ɪn ən əˈbændənd ˈbɪldɪŋ/","vi":"abandonné(e), délaissé(e), déserté(e)","example_vi":"Il s'est caché dans un bâtiment abandonné.","connect":"","image":"1qnmgG_lKieEMZU5tBWIyAmrUWBlmc5bu"},
+  {"id":3,"word":"ability","type":"(n)","ipa":"/ə'biliti/","cefr_level":"B1","en":"the fact that somebody/something is able to do something","example_en":"She has the ability to manage a business.","ipa_example":"/ʃiː həz ðiː əˈbɪlɪtɪ tə ˈmænɪʤ ə ˈbɪznɪs/","vi":"capacité","example_vi":"Elle a la capacité de gérer une entreprise.","connect":"","image":"1F-fLdVhraptKI26g3SqNy2WRBsS2oaKY"},
+  {"id":4,"word":"able","type":"(adj)","ipa":"/'eibl/","cefr_level":"A2","en":"intelligent or good at what you do","example_en":"I am able to swim across the river.","ipa_example":"/aɪ æm ˈeɪbl tə swɪm əˈkrɒs ðə ˈrɪvə/","vi":"capable","example_vi":"Je suis capable de nager à travers la rivière.","connect":"","image":"1f2yqtdmPoaMYa58Bb5toHZHW9agBgOKr"},
+  {"id":5,"word":"about","type":"(adv, prep)","ipa":"/ə'baut/","cefr_level":"A1","en":"on the subject of, or connected with","example_en":"I don't care about your past.","ipa_example":"/aɪ dəʊnt keə əˈbaʊt jə pɑːst/","vi":"À propos","example_vi":"Je me fiche de ton passé.","connect":"","image":"1w17P84DUU3I4Ohz0uKEnB76VcK_RYx8M"},
+  {"id":6,"word":"above","type":"(adv)","ipa":"/ə'bʌv/","cefr_level":"A1","en":"at or to a higher place or position than something/somebody","example_en":"She lives in an apartment above us.","ipa_example":"/ʃiː laɪvz ɪn ən əˈpɑːtmənt əˈbʌv ʌs/","vi":"dessus","example_vi":"Elle vit dans un appartement au-dessus de nous.","connect":"","image":"1HZmUIdz0bHnJTzImh33HHNef2EQLtUfL"},
+  {"id":7,"word":"abroad","type":"(adv)","ipa":"/ə'brɔ:d/","cefr_level":"B1","en":"in or to a foreign country","example_en":"I plan to go abroad in November.","ipa_example":"/aɪ plæn tə gəʊ əˈbrɔːd ɪn nəʊˈvembə/","vi":"à l’étranger","example_vi":"Je prévois de partir à l'étranger en novembre.","connect":"","image":"1M4B8uSD6tDoerweW5HKH-M49PpuwOY03"},
+  {"id":8,"word":"absence","type":"(n)","ipa":"/'æbsəns/","cefr_level":"B2","en":"a time when someone is not at work or at the place they are expected to be","example_en":"I don't know the reason for her absence.","ipa_example":"/aɪ dəʊnt nəʊ ðə ˈriːzn fə hɜː ˈæbsəns/","vi":"absence","example_vi":"Je ne connais pas la raison de son absence.","connect":"","image":"1LKDQfDISFfEYrn9Yvumul9X1ZHG1OOo7"},
+  {"id":9,"word":"absent","type":"(adj)","ipa":"/'æbsənt/","cefr_level":"B1","en":"not in a place because of illness, etc","example_en":"He is often absent from school.","ipa_example":"/hiː ɪz ˈɒfn ˈæbsənt frɒm skuːl/","vi":"absent(e), manquant(e)","example_vi":"Il est souvent absent de l'école.","connect":"","image":"1Jb2cG3h4u1I5HCbTqJMT9EcHWyxniNGL"},
+  {"id":10,"word":"absolute","type":"(adj)","ipa":"/'æbsəlu:t/","cefr_level":"B2","en":"very great or to the largest degree possible, used when expressing a strong opinion","example_en":"I need absolute silence when I'm working.","ipa_example":"/aɪ niːd ˈæbsəluːt ˈsaɪləns wen aɪm ˈwɜːkɪŋ/","vi":"absolu","example_vi":"J'ai besoin d'un silence absolu lorsque je travaille.","connect":"","image":"1awfrUaKKrgjkjoayysKHmwS7lB2HG879"},
+  {"id":11,"word":"absolutely","type":"(adv)","ipa":"/'æbsəlu:tli/","cefr_level":"B1","en":"used to emphasize that something is completely true","example_en":"Yes, You're absolutely right.","ipa_example":"/jes jə ˈæbsəluːtlɪ raɪt/","vi":"Tout à fait","example_vi":"Oui, tu as absolument raison.","connect":"","image":"1F6XkaTLlSThsSW2sTmbM6_TM1SJG9ToN"},
+  {"id":12,"word":"absorb","type":"(v)","ipa":"/əb'sɔ:b/","cefr_level":"B2","en":"to take in a liquid, gas or other substance from the surface or space around","example_en":"This paper does not absorb ink.","ipa_example":"/ðɪs ˈpeɪpə dʌz nɒt əbˈsɔːb ɪŋk/","vi":"absorber","example_vi":"Ce papier n'absorbe pas l'encre.","connect":"","image":"18HiiA6Die9fCQnFnMYILuqp4-464gPh8"},
+  {"id":13,"word":"abuse","type":"(v, n)","ipa":"/ə'bju:s/","cefr_level":"B2","en":"to use something for the wrong purpose in a way that is harmful or morally wrong","example_en":"He abuses his authority.","ipa_example":"/hiː əˈbjuːsɪz hɪz ɔːˈθɒrɪtɪ/","vi":"abuser de","example_vi":"Il abuse de son autorité.","connect":"","image":"1xC-8tNQ5VH9gQx0OAo-BJ1VYOB_wy4o6"},
+  {"id":14,"word":"academic","type":"(adj)","ipa":"/,ækə'demik/","cefr_level":"B2","en":"connected with education, especially studying in schools and universities","example_en":"His academic achievements are impressive.","ipa_example":"/hɪz ˌækəˈdemɪk əˈʧiːvmənts ə ɪmˈpresɪv/","vi":"académique","example_vi":"Ses réalisations académiques sont impressionnantes.","connect":"","image":"1801FKCgp7DE_gi9fZBoftLqmKumv_yZj"},
+  {"id":15,"word":"accent","type":"(n)","ipa":"/'æksənt/","cefr_level":"B1","en":"a way of pronouncing the words of a language that shows which country, area or social class a person comes from","example_en":"She speaks with a southern accent.","ipa_example":"/ʃiː spiːks wɪð ə ˈsʌðən ˈæksənt/","vi":"accent","example_vi":"Elle parle avec un accent du sud.","connect":"","image":"1BzBiBsNLGUIQ2n6rdQX_YBfpSbJslcZL"},
+  {"id":16,"word":"accept","type":"(v)","ipa":"/ək'sept/","cefr_level":"B1","en":"to take willingly something that is offered; to say ‘yes’ to an offer, invitation, etc","example_en":"Will you accept my invitation ?","ipa_example":"/wɪl jʊ əkˈsept maɪ ˌɪnvɪˈteɪʃən/","vi":"J’accepte","example_vi":"Accepterez-vous mon invitation ?","connect":"","image":"1m-hKcfOPKJEtwXLn1ZFJJma-G1InD0av"},
+  {"id":17,"word":"acceptable","type":"(adj)","ipa":"/ək'septəbl/","cefr_level":"B1","en":"agreed or approved of by most people in a society","example_en":"We want a solution that is acceptable to all parties.","ipa_example":"/wi wɒnt ə səˈluːʃən ðæt ɪz əkˈseptəbl tʊ ɔːl pɑːˈtiːz/","vi":"acceptable","example_vi":"Nous voulons une solution acceptable pour toutes les parties.","connect":"","image":"1SCBJPNIqZEPJFxBFeSV6qjBAggfOk922"}
 ];
 
 export async function POST(req) {
@@ -44,4 +51,34 @@ export async function POST(req) {
       headers: { 'Content-Type': 'application/json' },
     }
   );
+}
+
+export async function GET(req) {
+  const { searchParams } = new URL(req.url);
+  const cardId = searchParams.get('id');
+
+  // Find the card with the provided ID
+  const card = cards.find((card) => card.id === parseInt(cardId));
+
+  if (card) {
+    // Return the card's description (question/answer or both)
+    return new Response(
+      JSON.stringify({ card: {
+        example_vi: card.example_vi,
+        example_en: card.example_en,
+      }}),
+      {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  } else {
+    return new Response(
+      JSON.stringify({ message: 'Card not found' }),
+      {
+        status: 404,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
 }
--- app/cards/page.tsx
+++ app/cards/page.tsx
@@ -2,23 +2,53 @@
 import { useState, useEffect } from 'react';
 
 // Define the type for the card
+
+
 type Card = {
   id: number;
-  question: string;
-  answer: string;
+  example_en: string;
+  example_vi: string;
 };
 
 export default function CardApp() {
+  const defaultLotSize = 10;
+
   const [currentCard, setCurrentCard] = useState<Card | null>(null);
-  const [seenCards, setSeenCards] = useState(new Set());
-  const [repetitionCards, setRepetitionCards] = useState(new Set());
-  const [progress, setProgress] = useState({totalSeenCards: "?", totalCards: "?"});
+  const [seenCards, setSeenCards] = useState(new Set<number>());
+  const [repetitionCards, setRepetitionCards] = useState(new Set<number>());
+  const [progress, setProgress] = useState({ totalSeenCards: "?", totalCards: "?" });
 
   useEffect(() => {
-    const storedSeenCards = new Set(JSON.parse(localStorage.getItem('seenCards') || '[]'));
-    const storedRepetitionCards = new Set(JSON.parse(localStorage.getItem('repetitionCards') || '[]'));
+    // Fetch data from localStorage
+    const storedSeenCards = new Set<number>(JSON.parse(localStorage.getItem('seenCards') || '[]'));
+    const storedRepetitionCards = new Set<number>(JSON.parse(localStorage.getItem('repetitionCards') || '[]'));
     setSeenCards(storedSeenCards);
     setRepetitionCards(storedRepetitionCards);
+
+    // If there are seen cards, get the last one
+    if (storedSeenCards.size > 0) {
+      const lastSeenCardId = Array.from(storedSeenCards)[storedSeenCards.size - 1];
+      console.log(`Last seen card ID: ${lastSeenCardId}`);
+
+      const fetchLastCard = async () => {
+        const response = await fetch(`/api/getCard?id=${lastSeenCardId}`, {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        const data = await response.json();
+
+        if (data.card) {
+          setCurrentCard(data.card);
+        }
+      };
+      fetchLastCard();
+    }
+
+    setProgress({
+          totalSeenCards: storedSeenCards.size,
+          totalCards: defaultLotSize,
+        });
   }, []);
 
   const fetchCard = async () => {
@@ -29,6 +59,8 @@ export default function CardApp() {
     });
     const data = await response.json();
 
+    console.log('API response:', data);
+
     if (data.message === 'All cards read!') {
       alert('You have read all the cards!');
       return;
@@ -36,6 +68,7 @@ export default function CardApp() {
 
     setCurrentCard(data.card);
     setProgress(data.progress);
+    console.log('Card set:', data.card);
 
     const updatedSeenCards = new Set(seenCards);
     updatedSeenCards.add(data.card.id);
@@ -58,9 +91,9 @@ export default function CardApp() {
     setRepetitionCards(new Set());
     setCurrentCard(null);
     setProgress({
-                    totalSeenCards: "?",
-                    totalCards: "?"
-                  })
+      totalSeenCards: "?",
+      totalCards: "?",
+    });
     localStorage.setItem('seenCards', JSON.stringify([]));
     localStorage.setItem('repetitionCards', JSON.stringify([]));
   };
@@ -68,51 +101,55 @@ export default function CardApp() {
   return (
     <div className="bg-gradient-to-r from-blue-400 to-blue-600 min-h-screen text-white flex flex-col items-center justify-center py-8 relative">
       {/* Seen Cards Counter */}
-      <div className="absolute top-4 left-4 p-4 bg-opacity-70 bg-gray-800 rounded-xl">
-        <p className="text-lg font-semibold">
+      <div className="absolute top-4 left-4 p-4 bg-opacity-70 bg-gray-800 rounded-xl sm:top-6 sm:left-6">
+        <p className="text-sm sm:text-lg font-semibold">
           Seen: {progress.totalSeenCards} / {progress.totalCards}
         </p>
       </div>
 
       {/* App Title */}
-      <div className="text-3xl font-bold mb-8">Flashcards App</div>
+      <div className="text-2xl sm:text-3xl font-bold mb-6 sm:mb-8 text-center">
+        Flashcards App
+      </div>
 
       {/* Card Container */}
-      <div className="w-96 h-96 bg-white rounded-xl shadow-lg overflow-hidden transform transition-all">
+      <div className="w-80 h-80 sm:w-96 sm:h-96 bg-white rounded-xl shadow-lg overflow-hidden transform transition-all">
         {currentCard ? (
           <div className="w-full h-full flex flex-col justify-center items-center p-4 bg-gray-100 rounded-xl">
-            <h2 className="text-2xl font-semibold text-blue-500 mb-4">{currentCard.question}</h2>
-            <p className="text-xl text-gray-800">{currentCard.answer}</p>
+            <h2 className="text-xl sm:text-2xl font-semibold text-blue-500 mb-4 text-center">
+              {currentCard.example_vi}
+            </h2>
+            <p className="text-base sm:text-xl text-gray-800 text-center">{currentCard.example_en}</p>
           </div>
         ) : (
-          <div className="w-full h-full flex justify-center items-center text-gray-500">
+          <div className="w-full h-full flex justify-center items-center text-gray-500 text-center">
             <p>No card selected. Click 'Get New Card' to start.</p>
           </div>
         )}
       </div>
 
       {/* Buttons */}
-      <div className="mt-8 space-x-4 flex justify-center items-center">
+      <div className="mt-6 sm:mt-8 space-y-4 sm:space-y-0 sm:space-x-4 flex flex-col sm:flex-row justify-center items-center">
         <button
           onClick={fetchCard}
-          className="px-6 py-3 bg-blue-500 hover:bg-blue-700 rounded-lg text-lg"
+          className="px-4 sm:px-6 py-2 sm:py-3 bg-blue-500 hover:bg-blue-700 rounded-lg text-base sm:text-lg"
         >
           Get New Card
         </button>
         <button
           onClick={markForRepetition}
           disabled={!currentCard}
-          className="px-6 py-3 bg-yellow-500 hover:bg-yellow-700 rounded-lg text-lg disabled:opacity-50"
+          className="px-4 sm:px-6 py-2 sm:py-3 bg-yellow-500 hover:bg-yellow-700 rounded-lg text-base sm:text-lg disabled:opacity-50"
         >
           Mark for Repetition
         </button>
       </div>
 
       {/* Restart Button */}
-      <div className="mt-8">
+      <div className="mt-6 sm:mt-8">
         <button
           onClick={resetCards}
-          className="px-6 py-3 bg-red-500 hover:bg-red-700 rounded-lg text-lg"
+          className="px-4 sm:px-6 py-2 sm:py-3 bg-red-500 hover:bg-red-700 rounded-lg text-base sm:text-lg"
         >
           Restart
         </button>
