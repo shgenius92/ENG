7f8ce43636e0a7a83dcaf863a8d62a70fa037732
yama-consulting <backoffice@yamaconsulting.fr>
2025-01-28 00:51:05 +0100

    commit

--- app/cards2/page.tsx
+++ app/cards2/page.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import { Lock } from "lucide-react";
 
 const CardPage = () => {
-  // Create an array with 6 buckets
+  // Create an array with 30 buckets
   const buckets = Array.from({ length: 30 }, (_, index) => index + 1);
 
   return (
     <div className="min-h-screen bg-gray-100 py-8 flex items-center">
-      <div className="mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+      <div className="mx-auto grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-5 gap-2">
         {buckets.map((bucket) => (
           <div
             key={bucket}
-            className="bg-white rounded-lg shadow-md flex flex-col items-center justify-center m-2 w-32 h-40"
+            className="bg-white rounded-lg shadow-md flex flex-col items-center justify-center m-2 w-full sm:w-30 h-40"
           >
             <div className="w-full p-2 flex justify-center bg-gray-100 rounded-t-lg">
               <h2 className="text-xs font-semibold text-center text-gray-700">Bucket {bucket}</h2>
