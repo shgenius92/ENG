5755c308f5f824bfcc71cde6fd54d2fa1ef23e91
yama-consulting <backoffice@yamaconsulting.fr>
2025-01-28 19:05:58 +0100

    commit

--- app/cards2/page.tsx
+++ app/cards2/page.tsx
@@ -7,14 +7,16 @@ const CardPage = () => {
 
   return (
     <div className="min-h-screen bg-gray-100 py-8 flex items-center">
-      <div className="mx-auto grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-5 gap-2">
+      <div className="mx-auto grid grid-cols-5 gap-2">
         {buckets.map((bucket) => (
           <div
             key={bucket}
-            className="bg-white rounded-lg shadow-md flex flex-col items-center justify-center m-2 w-full sm:w-30 h-40"
+            className="bg-white rounded-lg shadow-md flex flex-col items-center justify-center m-2 w-full sm:w-28 h-40"
           >
             <div className="w-full p-2 flex justify-center bg-gray-100 rounded-t-lg">
-              <h2 className="text-xs font-semibold text-center text-gray-700">Bucket {bucket}</h2>
+              <h2 className="text-xs font-semibold text-center text-gray-700">
+                Bucket {bucket}
+              </h2>
             </div>
 
             <div className="flex items-center justify-center flex-grow">
